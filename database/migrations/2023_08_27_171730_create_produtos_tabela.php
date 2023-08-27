1614d2b7f2ef552a596f37fb9a6002f03b13634f
MARCELO COLLADO <90863579+Collado66@users.noreply.github.com>
2023-08-27 15:09:10 -0300

    breeze

---
+++ database/migrations/2023_08_27_171730_create_produtos_tabela.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosTabela extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos_tabela', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos_tabela');
+    }
+}
